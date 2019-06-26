83ea46cf2f0b54cd508fb7c4c51dca5cb4000ecf
irene-gomez <irene@irenegf.com>
2019-06-26 13:14:37 +0200

    Save mood and message into state

--- src/components/AddMood/index.js
+++ src/components/AddMood/index.js
@@ -1,37 +1,115 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
-const AddMood = () => {
-    return (
-        <section className="main-mood">
-            <form>
-                <div>
-                    <label htmlFor="date">Fecha</label>
-                    <input type="date" name="date" id="date"/>
-                </div>
+class AddMood extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            moodData: {
+                date: '',
+                mood: '',
+                message: ''
+            },
+            messageHidden: 'hidden',
+            currentDate: new Date(),
+        };
+        this.handleRadioChange = this.handleRadioChange.bind(this);
+        this.handleTextareaChange = this.handleTextareaChange.bind(this);
+    }
 
-                <div>
-                    <p>Estado</p>
-                    <input type="checkbox" name="mood" id="mood" value=":)" />
-                    <label htmlFor="mood">:)</label>
-                    <input type="checkbox" name="mood" id="mood" value=":(" />
-                    <label htmlFor="mood">:(</label>
-                </div>
+    handleRadioChange(event) {
+        const { value } = event.currentTarget;
+        this.saveMood(value);
+        this.showMessage(value);
+    }
 
-                <div>
-                    <label htmlFor="message">Mensaje</label>
-                    <textarea name="message" id="message"></textarea>
-                </div>
-                <p>
-                    <Link to="/">Home</Link>
-                </p>
-                <p>
-                    <Link to="/">Cancelar</Link>
-                </p>
-            </form>
-        </section>
-    );
+    handleTextareaChange(event) {
+        const { value } = event.currentTarget;
+        this.setState(prevState => {
+            return {
+                moodData: {
+                    ...prevState.moodData,
+                    message: value
+                }
+            };
+        });        
+    }
+
+    saveMood(data) {
+        this.setState(prevState => {
+            return {
+                moodData: {
+                    ...prevState.moodData,
+                    mood: data
+                }
+            };
+        });
+    }
+
+    showMessage(data) {
+        if(data === ':)') {
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    messageHiden: ''
+                };
+            });
+        } else {
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    messageHidden: 'hidden'
+                };
+            });
+        }
+    }
+
+    render() {
+        const { messageHiden } = this.state;
+        return (
+            <section className="main-mood">
+                <form>
+                    <div>
+                        <label htmlFor="date">Fecha</label>
+                        <input type="date" name="date" id="date" />
+                    </div>
+
+                    <div>
+                        <p>Estado</p>
+                        <input
+                            type="radio"
+                            name="mood"
+                            id="happyMood"
+                            value=":)"
+                            onChange={this.handleRadioChange}
+                        />
+                        <label htmlFor="happyMood">:)</label>
+
+                        <input
+                            type="radio"
+                            name="mood"
+                            id="sadMood"
+                            value=":("
+                            onChange={this.handleRadioChange}
+                        />
+                        <label htmlFor="sadMood">:(</label>
+                    </div>
+
+                    <div className={`${messageHiden}`}>
+                        <label htmlFor="message">Mensaje</label>
+                        <textarea name="message" id="message" onChange={this.handleTextareaChange} />
+                    </div>
+                    <p>
+                        <Link to="/">Home</Link>
+                    </p>
+                    <p>
+                        <Link to="/">Cancelar</Link>
+                    </p>
+                </form>
+            </section>
+        );
+    }
 }
- 
+
 export default AddMood;
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import AddMood from '../AddMood';
 
 import './styles.scss';
 
-function App() {
-    return (
-        <div className="App">
-            <Switch>
-				<Route exact path="/" component={Home} />
-				<Route path="/add-mood" component={AddMood} />
-			</Switch>
-        </div>
-    );
+class App extends Component {
+    constructor(props) {
+		super(props);
+		
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/add-mood" component={AddMood} />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
