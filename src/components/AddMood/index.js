99eba60330db3df42f275866cf2feb65b76cb1fe
irene-gomez <irene@irenegf.com>
2019-06-26 11:14:18 +0200

    Add Home and AddMood components

---
+++ src/components/AddMood/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const AddMood = () => {
+    return (
+        <p>AddMood</p>
+    );
+}
+ 
+export default AddMood;
---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.scss';
+
+const Home = () => {
+    return (
+        <p>Home</p>
+    );
+}
+ 
+export default Home;
