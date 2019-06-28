f4366aedc2e19996804c4faec138a5e8a4269ad4
irene-gomez <irene@irenegf.com>
2019-06-28 13:38:37 +0200

    Create docs directory

---
+++ docs/precache-manifest.bd9b5005e802675c8d4b8658fe58a5be.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe28c2d9698aed4bd3551e845c994a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1422990da56d5c6106b6",
+    "url": "./static/css/main.9ee3b83a.chunk.css"
+  },
+  {
+    "revision": "942c78c75cad75419fd0",
+    "url": "./static/js/2.35182350.chunk.js"
+  },
+  {
+    "revision": "1422990da56d5c6106b6",
+    "url": "./static/js/main.dbacd2e1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "786eb9d8f94ae38dfccbe55b45609378",
+    "url": "./static/media/add.786eb9d8.png"
+  },
+  {
+    "revision": "d3ee7a7f34ad46880802edfa10fd95f5",
+    "url": "./static/media/happyFace.d3ee7a7f.png"
+  },
+  {
+    "revision": "1e4946d2c769397b20b133551934355e",
+    "url": "./static/media/sadFace.1e4946d2.png"
+  }
+]);
