34fca73da04c170f4cc14b8bc29dfca872293186
irene-gomez <irene@irenegf.com>
2019-06-28 13:37:26 +0200

    Add tooltip in hover face

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
-const faceMood = props => {
-    console.log(props);
-    if (props === ':)') {
+const faceMood = mood => {
+    if (mood === ':)') {
         return 'happy';
     } else {
         return 'sad';
     }
+};
+
+const showDate = date => {
+    const year = date.slice(0, 4);
+    const month = date.slice(5, 7);
+    const day = date.slice(8);
+    return `${day}-${month}-${year}`;
 };
 
 const Home = props => {
@@ -27,10 +33,13 @@ const Home = props => {
                         {historyMood.map((mood, index) => (
                             <li key={index} className="mood__item">
                                 <div
-                                    className={`face face--${faceMood(
-                                        mood.mood
-                                    )}`}
-                                />
+                                    className={`face face--${faceMood(mood.mood)}`}
+                                >
+                                    <div className="wrapper__tooltip">
+                                        <p className="tooltip">{showDate(mood.date)}</p>
+                                        <p className="tooltip">{mood.message}</p>
+                                    </div>
+                                </div>
                             </li>
                         ))}
                     </ul>
