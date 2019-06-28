d975f960682723794930d8279bf31c1d26a5bc08
irene-gomez <irene@irenegf.com>
2019-06-28 11:48:38 +0200

    Add styles Home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,21 +3,40 @@ import { Link } from 'react-router-dom';
 
 import './styles.scss';
 
+const faceMood = props => {
+    console.log(props);
+    if (props === ':)') {
+        return 'happy';
+    } else {
+        return 'sad';
+    }
+};
+
 const Home = props => {
     const { historyMood } = props;
     return (
         <Fragment>
             <header className="main-header">
-                <Link to="/add-mood">AddMood</Link>
+                <Link to="/add-mood">
+                    <div className="add__mood" />
+                </Link>
             </header>
             <main className="main-content">
-                <ul className="mood__list">
-                    {historyMood.map((mood, index) => (
-                        <li key={index} className="mood__item">
-                            {mood.mood}
-                        </li>
-                    ))}
-                </ul>
+                {historyMood.length !== 0 ? (
+                    <ul className="mood__list">
+                        {historyMood.map((mood, index) => (
+                            <li key={index} className="mood__item">
+                                <div
+                                    className={`face face--${faceMood(
+                                        mood.mood
+                                    )}`}
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="user-info">Todavía no se ha añadido ningún estado.</p>
+                )}
             </main>
         </Fragment>
     );
